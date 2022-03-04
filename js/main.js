1d5518a917a8f55d02b3ebd1449b94b12f418092
Aliaksei <brel.2000@mail.ru>
2022-03-04 11:03:46 +0300

    create form

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
-$(".menu-btn").on("click", () => {
-    $(".header__menuContent").toggleClass("active");
-    $(".menu-btn").toggleClass("menu-btn_active");
-    $("body").toggleClass("lock");
-});
+const menuBtn = document.querySelector('.menu-btn'),
+    headerMenuContent = document.querySelector('.header__menuContent'),
+    body = document.getElementsByTagName('body')[0];
 
-$(".linkRegAuth").on("click", () => {
-    $("#headerBtn").fadeToggle(500).css("display", "flex");
-});
+const linkRegAuth = document.querySelector('.linkRegAuth'),
+    headerBtn = document.querySelector('#headerBtn');
+
+const btnRegistr = document.querySelector('.btnRegistr'),
+    regForm = document.querySelector('.reg__form');
+
+const btnEntrance = document.querySelector('.btnEntrance'),
+    autoForm = document.querySelector('.auto__form');
+
+menuBtn.addEventListener('click', () => {
+    headerMenuContent.classList.toggle('active');
+    menuBtn.classList.toggle('menu-btn_active')
+    body.classList.toggle('lock');
+})
+
+linkRegAuth.addEventListener('click', () => {
+    if (headerBtn.style.display == 'none') {
+        headerBtn.style.display = 'flex';
+    } else {
+        headerBtn.style.display = 'none';
+    }
+})
+
+btnRegistr.addEventListener('click', () => {
+    regForm.classList.remove('noActive');
+    body.classList.toggle('lock');
+})
+
+btnEntrance.addEventListener('click', () => {
+    autoForm.classList.remove('noActive');
+    body.classList.toggle('lock');
+})
+
+function closeForm(elem, elemClose, e) {
+    let target = e.target;
+    if (!target.closest(elem)) {
+        $(elemClose).addClass('noActive')
+        body.classList.remove('lock');
+    }
+}
+
+autoForm.addEventListener('click', (e) => { closeForm('.auto__body', '.auto__form', e); })
+regForm.addEventListener('click', (e) => { closeForm('.reg__body', '.reg__form', e); })
